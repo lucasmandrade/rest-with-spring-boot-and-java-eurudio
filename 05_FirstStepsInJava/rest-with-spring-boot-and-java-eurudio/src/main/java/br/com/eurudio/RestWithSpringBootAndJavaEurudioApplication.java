3bfa51d639fcebdd2bf8191f3743f72c21673ef7
Lucas Andrade <lucasm748+git@gmail.com>
2023-03-22 23:02:07 -0300

    Firt Steps in Java With Spring Boot 3

---
+++ 05_FirstStepsInJava/rest-with-spring-boot-and-java-eurudio/src/main/java/br/com/eurudio/RestWithSpringBootAndJavaEurudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.eurudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaEurudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaEurudioApplication.class, args);
+	}
+
+}
